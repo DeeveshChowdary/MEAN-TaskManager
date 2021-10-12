024fb8967c0478d9e10b7d29dc3ae7d37b5e3024
Deevesh Chowdary <42311355+DeeveshChowdary@users.noreply.github.com>
2021-10-12 09:04:58 +0530

    Add files via upload

---
+++ frontend/src/app/pages/signup-page/signup-page.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth.service';
+import { HttpResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-signup-page',
+  templateUrl: './signup-page.component.html',
+  styleUrls: ['./signup-page.component.scss']
+})
+export class SignupPageComponent implements OnInit {
+
+  constructor(private authService:AuthService,private router:Router) { }
+
+  ngOnInit() {
+  }
+  onSignupButtonClicked(email: string, password: string) {
+    this.authService.signup(email, password).subscribe((res: HttpResponse<any>) => {
+     
+    });
+  }
+
+}
